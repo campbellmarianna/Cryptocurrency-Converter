94ed8f496667e1a30c69b6c7caeafc427b1ccef3
Marianna <mckd2018@gmail.com>
2019-10-16 11:11:14 -0700

    [add] setup

---
+++ app/components/Button/ButtonWithInput.js
@@ -0,0 +1,3 @@
+import { } from 'react-native';
+
+cost ButtonWithInput = () => <View></View>
---
+++ app/index.js
@@ -0,0 +1,20 @@
+// entry point
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Let's build a Cryptocurrency Converter from app/index.js!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
